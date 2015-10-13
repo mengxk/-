6e3ad4076d123b90199f9c449449c30e036b5e11
github1005682757 <1005682757@qq.com>
2015-10-13 15:39:46 +0800

    lianmeng

--- js/main.js
+++ js/main.js
@@ -27,4 +27,68 @@ $(document).ready(function() {
 		$("body").scrollTop(0);
 		$(this).fadeOut();
 	});
+	/*筛选*/
+	$("body").on("click", ".box-nav a,.box-nav span", function(e){
+		var $this = $(this);
+		var $next = $this.siblings("ul");
+		if($next.length == 1){
+			if($next.is(":hidden")){
+				$next.slideDown();
+			}else{
+				$next.slideUp();
+			}
+		}else{
+			$this.closest(".item").find("span").text($this.text()).addClass("color-f30");
+			e.preventDefault();
+		}
+	});
+	/*tabs*/
+	$(".ui-tabs .ui-tabs-nav a:eq(0)").trigger("click");
+	//轮播图
+	(function(){
+		var $banner = $(".banner ul");
+		var $num = $banner.next(".num");
+		var $bannerItem = $banner.find("li");
+		$num.text("1/" + $bannerItem.length);
+		$bannerItem.width($bannerItem.width());
+		$banner.width($bannerItem.length * $bannerItem.width());
+		window.addEventListener("load", function(){
+			$banner.closest(".banner").height($banner.height());
+		});
+		$bannerItem.each(function(e){
+			$(this).data("data-index", (e + 1)); 
+		});
+		
+		//向左滑
+		$("body").on("swipeleft", ".banner li", function(e){
+			if(!$banner.is(":animated")){
+				var $this = $(this);
+				var index = $this.index();
+				var width = $this.width();
+				$banner.animate({
+					"left": -width + "px"
+				}, 1000, function(){
+					$this.appendTo($banner);
+					$banner.css("left", 0);
+					$num.text($this.data("data-index") + 1 + "/" + $bannerItem.length);
+				});
+			}
+		});
+		//向右滑
+		$("body").on("swiperight", ".banner li", function(e){
+			if(!$banner.is(":animated")){
+				var $this = $(this);
+				var index = $this.index();
+				var width = $this.width();
+				$banner.css("left", -width + "px");
+				$banner.find("li:last-child").prependTo($banner);
+				$banner.animate({
+					"left": "0px"
+				}, 1000, function(){
+					var num = $this.data("data-index") - 1 == 0 ? 3 : ($this.data("data-index") - 1);
+					$num.text(num + "/" + $bannerItem.length);
+				});
+			}
+		});
+	})();
 });
